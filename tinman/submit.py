df14d5b661e8dce747df866b679bdd4e911ac7d3
theoreticalbts <theoreticalbts@users.noreply.github.com>
2018-02-09 15:51:37 -0500

    Implement tinman submit --realtime #48

--- tinman/submit.py
+++ tinman/submit.py
@@ -57,6 +57,57 @@ class CachedDgpo(object):
             self.last_refresh = now
         return self.dgpo
 
+def wait_for_real_time(when):
+    while True:
+        rtc_now = datetime.datetime.utcnow()
+        if rtc_now >= when:
+            break
+        time.sleep(0.4)
+
+def generate_blocks(steemd, args, cached_dgpo=None, now=None, produce_realtime=False):
+    if args["count"] <= 0:
+        return
+
+    miss_blocks = args.get("miss_blocks", 0)
+
+    if not produce_realtime:
+        steemd.debug_node_api.debug_generate_blocks(
+            debug_key="5JNHfZYKGaomSFvd4NUdQ9qMcEAC43kujbfjueTHpVapX1Kzq2n",
+            count=args["count"],
+            skip=0,
+            miss_blocks=miss_blocks,
+            edit_if_needed=False,
+            )
+        return
+    dgpo = cached_dgpo.get()
+    now = dgpo["time"]
+
+    head_block_time = datetime.datetime.strptime(dgpo["time"], "%Y-%m-%dT%H:%M:%S")
+    next_time = head_block_time + datetime.timedelta(seconds=3*(1+miss_blocks))
+
+    print("wait_for_real_time( {} )".format(next_time))
+    wait_for_real_time(next_time)
+    print("calling debug_generate_blocks, miss_blocks={}".format(miss_blocks))
+    steemd.debug_node_api.debug_generate_blocks(
+           debug_key="5JNHfZYKGaomSFvd4NUdQ9qMcEAC43kujbfjueTHpVapX1Kzq2n",
+           count=1,
+           skip=0,
+           miss_blocks=miss_blocks,
+           edit_if_needed=False,
+           )
+    print("entering loop")
+    for i in range(1, args["count"]):
+        next_time += datetime.timedelta(seconds=3)
+        wait_for_real_time(next_time)
+        steemd.debug_node_api.debug_generate_blocks(
+               debug_key="5JNHfZYKGaomSFvd4NUdQ9qMcEAC43kujbfjueTHpVapX1Kzq2n",
+               count=1,
+               skip=0,
+               miss_blocks=0,
+               edit_if_needed=False,
+               )
+    return
+
 def main(argv):
 
     parser = argparse.ArgumentParser(prog=argv[0], description="Submit transactions to Steem")
@@ -66,6 +117,7 @@ def main(argv):
     parser.add_argument("-f", "--fail-file", default="-", dest="fail_file", metavar="FILE", help="File to write failures, - for stdout, die to quit on failure")
     parser.add_argument("-c", "--chain-id", default="", dest="chain_id", metavar="CID", help="Specify chain ID")
     parser.add_argument("--timeout", default=5.0, type=float, dest="timeout", metavar="SECONDS", help="API timeout")
+    parser.add_argument("--realtime", dest="realtime", action="store_true", help="Wait when asked to produce blocks in the future")
     args = parser.parse_args(argv[1:])
 
     die_on_fail = False
@@ -87,6 +139,7 @@ def main(argv):
     backend = SteemRemoteBackend(nodes=[args.testserver], appbase=True, min_timeout=timeout, max_timeout=timeout)
     steemd = SteemInterface(backend)
     sign_transaction_exe = args.sign_transaction_exe
+    produce_realtime = args.realtime
 
     cached_dgpo = CachedDgpo(steemd=steemd)
 
@@ -103,13 +156,7 @@ def main(argv):
 
         try:
             if cmd == "wait_blocks":
-                steemd.debug_node_api.debug_generate_blocks(
-                    debug_key="5JNHfZYKGaomSFvd4NUdQ9qMcEAC43kujbfjueTHpVapX1Kzq2n",
-                    count=args["count"],
-                    skip=0,
-                    miss_blocks=args.get("miss_blocks", 0),
-                    edit_if_needed=False,
-                    )
+                generate_blocks(steemd, args, cached_dgpo=cached_dgpo, produce_realtime=produce_realtime)
                 cached_dgpo.reset()
             elif cmd == "submit_transaction":
                 tx = args["tx"]
