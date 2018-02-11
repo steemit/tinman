3b9ce917c46c48bce2f9f7a4fa633301bbde1900
theoreticalbts <theoreticalbts@users.noreply.github.com>
2018-02-11 00:11:23 -0500

    Update block_signing_key for witnesses #49

--- tinman/txgen.py
+++ tinman/txgen.py
@@ -69,7 +69,7 @@ def update_witnesses(conf, keydb, name):
         yield {"operations" : [["witness_update",{
             "owner" : name,
             "url" : "https://steemit.com/",
-            "block_signing_key" : keydb.get_pubkey(name, "block"),
+            "block_signing_key" : "TST6LLegbAgLAy28EHrffBVuANFWcFgmqRMW13wBmTExqFE9SCkg4",
             "props" : {},
             "fee" : amount(0),
            }]],
