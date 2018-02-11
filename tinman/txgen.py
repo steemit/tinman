4c9dd927405d3091c6c34bacd3908ea3df221b2a
theoreticalbts <theoreticalbts@users.noreply.github.com>
2018-02-11 00:13:43 -0500

    Generate 1 billion blocks #49

--- tinman/txgen.py
+++ tinman/txgen.py
@@ -254,7 +254,7 @@ def build_actions(conf):
         yield ["submit_transaction", {"tx" : tx}]
     for tx in vote_accounts(conf, keydb, "elector", "init"):
         yield ["submit_transaction", {"tx" : tx}]
-    yield ["wait_blocks", {"count" : 50}]
+    yield ["wait_blocks", {"count" : 1000000000}]
 
     return
 
