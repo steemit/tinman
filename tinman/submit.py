159b07e058f94a28774022ceace630262650c2e5
Donovan Walker <donovan@donovanwalker.com>
2018-08-22 17:38:39 -0500

    strip whitespace from chain id

--- tinman/submit.py
+++ tinman/submit.py
@@ -144,7 +144,7 @@ def main(argv):
     cached_dgpo = CachedDgpo(steemd=steemd)
 
     if args.chain_id != "":
-        chain_id = args.chain_id
+        chain_id = args.chain_id.strip()
     else:
         chain_id = None
 
